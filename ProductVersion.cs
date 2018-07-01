cb66f2c9fc55929d4be14cbc6fae0ac19be7bc51
Eyaz Rehman <eyaz.rehman@indigogames.ca>
2018-07-01 12:19:48 -0400

    Make setters public to allow deserialization

--- ProductVersion.cs
+++ ProductVersion.cs
@@ -12,10 +12,10 @@ namespace IndigoVersion
 {
     public class ProductVersion
     {
-        public int Major { get; private set; }
-        public int Minor { get; private set; }
-        public int Build { get; private set; }
-        public int Revision { get; private set; }
+        public int Major { get; set; }
+        public int Minor { get; set; }
+        public int Build { get; set; }
+        public int Revision { get; set; }
 
         public ProductVersion()
         {
